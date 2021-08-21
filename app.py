6ed07132e0f63b1c8176bb84ba603b201676bd02
a10923034 <83624185+a10923034@users.noreply.github.com>
2021-08-21 21:14:17 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app=Flask(__name__) #__name__代表目前執行的模組
+
+@app.route("/") #函式的裝飾
+def index():
+    return render_template("index.html")
+
+if __name__=="__main__": #如果以主程式執行
+    app.run(debug=True) #立刻啟動伺服器
